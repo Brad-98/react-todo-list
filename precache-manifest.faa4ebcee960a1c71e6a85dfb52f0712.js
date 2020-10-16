eb5822e4363858eb2f781e8a717bab0df3f61e1f
Bradley Modern <modern.bradley@gmail.com>
2020-10-16 19:52:07 +0100

    Updates

---
+++ precache-manifest.faa4ebcee960a1c71e6a85dfb52f0712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79080b7b3adec78679ab5fd232e528a5",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "2ce4a5bef93f3ea2f84a",
+    "url": "/react-todo-list/static/css/main.b51c83da.chunk.css"
+  },
+  {
+    "revision": "7b666882a62bba9880f3",
+    "url": "/react-todo-list/static/js/2.a46155fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-list/static/js/2.a46155fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce4a5bef93f3ea2f84a",
+    "url": "/react-todo-list/static/js/main.efb85b2b.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  }
+]);
